80992aa3e4c620179501438f8287649ac588dd61
Blaire Baker <bakerblaire@gmail.com>
2020-12-29 16:10:07 -0600

    done

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+    def display_artist(song)
+        if song.artist_id == nil
+            link_to "Add Artist", edit_song_path(song)
+        else
+            link_to song.artist_name, artist_path(song.artist_id)
+        end
+    end
 end
